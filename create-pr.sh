3917d65fbadae22bd09b70a947adf50cff3cc7fe
Will Martin <will@storyscript.io>
2020-01-08 15:46:10 +0100

    Remove old pipeline that released studio

--- create-pr.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ash
-
-set -eu
-
-curl -H "Authorization: Bearer ${TOKEN}" -X "POST" -d '{"title": "New Release", "body": "", "head": "release" , "base": "dev"}' https://api.github.com/repos/williammartin/studio/pulls
