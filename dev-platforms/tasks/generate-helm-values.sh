35fe16906111f1d8ae6db47e784d6bf30979638e
Will Martin <will@storyscript.io>
2020-01-09 16:19:21 +0100

    Add github auth to helm install

--- dev-platforms/tasks/generate-helm-values.sh
+++ dev-platforms/tasks/generate-helm-values.sh
@@ -5,10 +5,14 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
 terraform_dir="${workspace_dir}/terraform"
 helm_dir="${workspace_dir}/helmconfig"
+github_app_dir="${workspace_dir}/github-apps-pool"
 
 name=$(cat "${terraform_dir}/name")
 
 lb_ip="$(jq -r .load_balancer_ip < "${terraform_dir}/metadata")"
+
+client_id="$(jq -r .client_id < "${github_app_dir}/metadata")"
+client_secret="$(jq -r .client_secret < "${github_app_dir}/metadata")"
 
 cat > "${helm_dir}/values.yml" <<EOF
 domain: ${name}.${DOMAIN}
@@ -20,6 +24,6 @@ nginx-ingress:
 
 auth:
   github:
-    client_id: foo
-    client_secret: bar
+    client_id: ${client_id}
+    client_secret: ${client_secret}
 EOF
