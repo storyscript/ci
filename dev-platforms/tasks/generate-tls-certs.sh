656820b5d611cc7bd28a4f9acc47da317c4e0ffc
Will Martin <will@storyscript.io>
2020-01-23 18:30:54 +0100

    Use pregenerated TLS certs for dev platforms

--- dev-platforms/tasks/generate-tls-certs.sh
+++ dev-platforms/tasks/generate-tls-certs.sh
@@ -1,29 +1,15 @@
-#!/bin/ash
+#!/bin/bash
 
 # INPUTS
 workspace_dir=$(pwd)
-terraform_dir="${workspace_dir}/platform-terraform"
 
 # OUTPUTS
 tls_certs_dir="${workspace_dir}/tls-certs"
 
-name=$(cat "${terraform_dir}/name")
-account_file=${workspace_dir}/account.json
-
-cat > ${account_file} <<EOF
-${GCP_CREDENTIALS_JSON}
+cat > "${tls_certs_dir}/fullchain.pem" <<EOF
+${TLS_FULL_CHAIN}
 EOF
 
-certbot certonly -n --agree-tos \
-  --dns-google \
-  --dns-google-credentials ${account_file} \
-  -m "infra@storyscript.com" \
-  -d "${name}.storyscript-ci.com" \
-  -d "*.${name}.storyscript-ci.com" \
-  --config-dir ${workspace_dir} \
-  --work-dir ${workspace_dir} \
-  --logs-dir ${workspace_dir}
-
-generated_cert_dir="${workspace_dir}/live/${name}.storyscript-ci.com"
-cp -p "${generated_cert_dir}/fullchain.pem" "${tls_certs_dir}/fullchain.pem"
-cp -p "${generated_cert_dir}/privkey.pem" "${tls_certs_dir}/privkey.pem"
+cat > "${tls_certs_dir}/privkey.pem" <<EOF
+${TLS_PRIV_KEY}
+EOF
