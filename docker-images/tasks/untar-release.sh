0441ae6fb245fd9f0510285c633cdd9f3b532933
Anukul Sangwan <anukulsangwan@icloud.com>
2020-02-27 20:29:15 +0530

    Document mysterious code

--- docker-images/tasks/untar-release.sh
+++ docker-images/tasks/untar-release.sh
@@ -7,8 +7,12 @@ workspace_dir="$(pwd)"
 release_dir="${workspace_dir}/release"
 
 pushd "${release_dir}" > /dev/null
+  # We don't know the name of the extracted directory (looks like {{repository}}-{{commit_hash}})
+  # so put it under `source` using `--one-top-level`
   tar -xf source.tar.gz --one-top-level
   pushd "source" > /dev/null
-    mv */* .
+    # Again, we don't know the name of the directory
+    # So use a wildcard to copy all files one folder down (inside {{repository}}-{{commit_hash}}) to the current directory (source)
+    mv ./*/* .
   popd > /dev/null
 popd > /dev/null
