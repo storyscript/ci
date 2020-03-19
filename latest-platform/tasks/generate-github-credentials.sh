c2a6b4250db2cda46cd8e7c1bf16e2f209c353e2
Anukul Sangwan <anukulsangwan@icloud.com>
2020-03-19 23:21:49 +0530

    latest-platform: add helpful note in generate-github-credentials

--- latest-platform/tasks/generate-github-credentials.sh
+++ latest-platform/tasks/generate-github-credentials.sh
@@ -10,3 +10,5 @@ jq -n \
   --arg client_id "$GITHUB_CLIENT_ID" \
   --arg client_secret "$GITHUB_CLIENT_SECRET" \
   '{client_id: $client_id, client_secret: $client_secret}' > "${github_credentials_dir}"/metadata
+# NOTE: since this file is used in a later task which is shared with latest-platform,
+# it is named "metadata" here to match the name used by pool-resource in latest-platform
