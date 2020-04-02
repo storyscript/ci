0a187a8caec85d80186f39b0188404fec6927ddc
Anukul Sangwan <anukulsangwan@icloud.com>
2020-04-02 16:16:06 +0530

    latest-platform: add slack signing secret

--- latest-platform/tasks/generate-app-credentials.sh
+++ latest-platform/tasks/generate-app-credentials.sh
@@ -11,6 +11,7 @@ jq -n \
   --arg github_client_secret "$GITHUB_CLIENT_SECRET" \
   --arg slack_client_id "$SLACK_CLIENT_ID" \
   --arg slack_client_secret "$SLACK_CLIENT_SECRET" \
+  --arg slack_signing_secret "$SLACK_SIGNING_SECRET" \
 '{
   github: {
     client_id: $github_client_id,
@@ -18,7 +19,8 @@ jq -n \
   },
   slack: {
     client_id: $slack_client_id,
-    client_secret: $slack_client_secret
+    client_secret: $slack_client_secret,
+    signing_secret: $slack_signing_secret
   }
 }' > "${app_credentials_dir}"/metadata
 # NOTE: since this file is used in a later task which is shared with latest-platform,
