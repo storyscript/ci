d51892083c368edc602932f2f07c1a06cf338e90
Anukul Sangwan <anukulsangwan@icloud.com>
2020-04-02 16:03:08 +0530

    latest-platform: switch back to parsing jq args from flags

--- latest-platform/tasks/generate-app-credentials.sh
+++ latest-platform/tasks/generate-app-credentials.sh
@@ -6,15 +6,20 @@ workspace_dir=$(pwd)
 # OUTPUTS
 app_credentials_dir="${workspace_dir}/app-credentials"
 
-jq -n '
-{
-  github:
-    client_id: env.GITHUB_CLIENT_ID,
-    client_secret: env.GITHUB_CLIENT_SERET
-  slack:
-    client_id: env.SLACK_CLIENT_ID
-    client_secret: env.SLACK_CLIENT_SECRET
-}
-' > "${app_credentials_dir}"/metadata
+jq -n \
+  --arg github_client_id "$GITHUB_CLIENT_ID" \
+  --arg github_client_secret "$GITHUB_CLIENT_SECRET" \
+  --arg slack_client_id "$SLACK_CLIENT_ID" \
+  --arg slack_client_secret "$SLACK_CLIENT_SECRET" \
+'{
+  github: {
+    client_id: $github_client_id,
+    client_secret: $github_client_secret
+  },
+  slack: {
+    client_id: $slack_client_id,
+    client_secret: $slack_client_secret
+  }
+}' > "${app_credentials_dir}"/metadata
 # NOTE: since this file is used in a later task which is shared with latest-platform,
 # it is named "metadata" here to match the name used by pool-resource in latest-platform
