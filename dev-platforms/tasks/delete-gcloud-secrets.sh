9289eb60bed1f7795833f1501a28ac4974911b12
Anukul Sangwan <anukulsangwan@icloud.com>
2020-04-05 13:25:41 +0530

    dev-platforms: delete-gcloud-secrets: add credentials

--- dev-platforms/tasks/delete-gcloud-secrets.sh
+++ dev-platforms/tasks/delete-gcloud-secrets.sh
@@ -2,8 +2,18 @@
 
 set -Eeuo pipefail
 
+workspace_dir=$(pwd)
+
 # shellcheck disable=SC2154
 env=$(cat "${ENV_NAME_FILE}")
+
+account_file=${workspace_dir}/account.json
+
+cat > "${account_file}" <<EOF
+${GCP_CREDENTIALS_JSON}
+EOF
+
+gcloud auth activate-service-account --key-file="${account_file}"
 
 gcloud secrets list --filter="labels.env:${env}" --format="json" | jq ".[] | .name" | \
 xargs -I secret_name gcloud secrets delete secret_name --quiet
